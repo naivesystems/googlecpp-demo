d7c211904718394e987ae80a9e0a84ebd1be46bb
Mengting Chen <cmengting@naivesystems.com>
2023-10-15 22:59:09 -0700

    test updated binaries

--- demo.cc
+++ demo.cc
@@ -10,7 +10,7 @@ struct Point {
 
 class Base {
   public:
-    Base(int n) { data_ = n; }
+    Base(int n) { data_ = n; }  // test
     explicit Base(int x, int y) { data_ = x + y; }
     virtual bool Equal(Base* other = nullptr) = 0;
 
