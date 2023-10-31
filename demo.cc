2621e8da3f790f5ad0c1be9b31f019bfe2784a98
Mengting Chen <cmengting@naivesystems.com>
2023-10-30 20:29:30 -0700

    test v0.1.20

--- demo.cc
+++ demo.cc
@@ -1,48 +1,9 @@
-#include <cstring>
-#include <iostream>
-#include <string>
-
-struct Point {
-    int x;
-    int y;
-    std::string name;
-};
-
 class Base {
-  public:
-    Base(int n) { data_ = n; }  // test
-    explicit Base(int x, int y) { data_ = x + y; }
-    virtual bool Equal(Base* other = nullptr) = 0;  // test
-
-  private:
-    int data_;
+ public:
+  virtual void someFunction();
 };
 
 class Derived : public Base {
-  public:
-    bool EqualDynamic(Base* other);
-    bool EqualStatic(Base* other);
-
-  private:
-    int data_;
+ public:
+  virtual void someFunction() override;
 };
-
-bool Derived::EqualDynamic(Base* other) {
-    Derived* that = dynamic_cast<Derived*>(other);
-    return this->data_ == that->data_;
-}
-
-bool Derived::EqualStatic(Base* other) {
-    Derived* that = static_cast<Derived*>(other);
-    return this->data_ == that->data_;
-}
-
-int main() {
-    Point p1, p2;
-    memset(&p1, 0, sizeof(p1));     // sizeof(varname)
-    memset(&p2, 0, sizeof(Point));  // sizeof(type)
-
-    float f = 3.5;
-    int c_cast = (int)f;
-    int cpp_cast = static_cast<int>(f);
-}
